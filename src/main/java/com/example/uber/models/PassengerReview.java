20aa0d9e5c085e2c2e38e48b0dc31e5e98d291ab
Paarth Gupta <i.m.paarthgupta@gmail.com>
2025-08-30 14:53:49 +0530

    added passenger review model as joined table

--- src/main/java/com/example/uber/models/PassengerReview.java
+++ src/main/java/com/example/uber/models/PassengerReview.java
@@ -1,5 +1,6 @@
 package com.example.uber.models;
 
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import lombok.Getter;
 import lombok.Setter;
@@ -8,5 +9,10 @@ import lombok.Setter;
 @Getter
 @Setter
 public class PassengerReview extends Review {
+
+    @Column(nullable = false)
     private String passengerReviewContent;
+
+    @Column(nullable = false)
+    private String passengerRating;
 }
